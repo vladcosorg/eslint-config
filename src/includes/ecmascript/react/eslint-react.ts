8d58ce93574eb350ce23fec93e4760122b4fc40b
Vlad Cos <vcosvic@gmail.com>
2025-10-16 21:25:27 +0300

    wip

--- src/includes/ecmascript/react/eslint-react.ts
+++ src/includes/ecmascript/react/eslint-react.ts
@@ -5,22 +5,12 @@ import react from '@eslint-react/eslint-plugin'
 import { type ExtendsElement } from '@eslint/config-helpers'
 
 export const eslintReact = defineConfig({
-  extends: [
-    react.configs['recommended-type-checked'] as unknown as ExtendsElement,
-  ],
+  extends: [react.configs.all as unknown as ExtendsElement],
   files: [GLOB_JSX, GLOB_TSX],
-  rules: {
-    '@eslint-react/no-children-prop': 2,
-    '@eslint-react/no-complex-conditional-rendering': 2,
-    '@eslint-react/no-useless-fragment': 2,
-    '@eslint-react/prefer-destructuring-assignment': 1,
-    '@eslint-react/prefer-read-only-props': 2,
-    '@eslint-react/prefer-shorthand-boolean': 2,
-    '@eslint-react/prefer-shorthand-fragment': 2,
-  },
+  rules: {},
   settings: {
     'react-x': {
-      version: '18.3.1',
+      version: '19.2.0',
     },
   },
 })
--- src/includes/tailwind.ts
+++ src/includes/tailwind.ts
@@ -1,26 +1,17 @@
-import eslintPluginReadableTailwind from 'eslint-plugin-readable-tailwind'
-import tailwindPlugin from 'eslint-plugin-tailwindcss'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 import { defineConfig } from 'eslint/config'
 
 import { GLOB_HTML, GLOB_TS, GLOB_TSX } from '@antfu/eslint-config'
 
 export const tailwind = defineConfig({
-  extends: [tailwindPlugin.configs['flat/recommended']],
   files: [GLOB_TSX, GLOB_TS, GLOB_HTML],
   plugins: {
-    'readable-tailwind': eslintPluginReadableTailwind,
+    'better-tailwindcss': eslintPluginBetterTailwindcss,
   },
 
   rules: {
-    'tailwindcss/no-custom-classname': 'off',
-    ...eslintPluginReadableTailwind.configs.warning.rules,
-    ...eslintPluginReadableTailwind.configs.error.rules,
-  },
-  settings: {
-    tailwindcss: {
-      // These are the default values but feel free to customize
-      callees: ['classnames', 'clsx', 'ctl', 'tw', 'tv', 'twMerge'],
-      cssFiles: [],
-    },
+    ...eslintPluginBetterTailwindcss.configs['recommended']?.rules,
+    'better-tailwindcss/enforce-consistent-variable-syntax': 'warn',
+    'better-tailwindcss/enforce-shorthand-classes': 'warn',
   },
 })
